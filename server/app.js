426a6adf285c0dcb9c9c389923721cad96e34267
Harsh <hvs8380@gmail.com>
2026-03-08 10:52:28 +0530

    fix

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use('/api/council', councilRoutes);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
